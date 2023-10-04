243f5289fc7dc5782b86ba889f59ff348a524cc9
Anantvasu200 <anant.1923en1183@kiet.edu>
2023-10-04 22:08:10 +0530

    Working on Functions

---
+++ function.js
@@ -0,0 +1,13 @@
+function multiply(x, y) {
+    return x * y;
+}
+
+console.log(multiply(4, 5));
+
+function toCelsius(fahrenheit) {
+    c=(5/9) * (fahrenheit-32);
+    return c
+}
+let value = toCelsius(82);
+console.log(value)
+
